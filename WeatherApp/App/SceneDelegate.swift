26a33357931a261e9d29735d3a7880d3924a610f
seba1549 <sebastian.maludzinski@gmail.com>
2024-07-10 16:42:58 +0200

    Initial Commit

---
+++ WeatherApp/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  WeatherApp
+//
+//  Created by Sebastian Maludziński on 10/07/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let mainViewController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = navigationController
+    }
+    
+}
+
---
+++ WeatherApp/HomeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Sebastian Maludziński on 10/07/2024.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController, UISearchBarDelegate {
+    
+    
+    // MARK: - Subviews
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = .minimal
+        searchBar.delegate = self
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "Miasto"
+        return searchBar
+    }()
+    
+    // MARK: - API ??
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    // MARK: - Methods
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        title = "WeatherApp"
+        
+        view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Tu będzie odpalane zapytanie do API o listę miast
+        print(searchText)
+    }
+    
+}
+
